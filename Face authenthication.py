0c6d551fb82325f899d0605bbc2993df7432cf56
DEO <deo_galagaran@yahoo.com>
2022-01-20 20:01:21 +0800

    Face authentication

--- Face authenthication.py
+++ Face authenthication.py
@@ -1,35 +1,51 @@
 import cv2 as cv
 import numpy as np
 
+
 face_cascade = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
-recognizer =  cv.face.LBPHFaceRecognizer_create()
+recognizer =  cv.face.LBPHFaceRecognizer_create();
 recognizer.read("recognizers/face-trainner.yml")
 
+
+
+font = cv.FONT_HERSHEY_DUPLEX
+
 camera = cv.VideoCapture(0)
-x
+
 while(True):
 #Capture frame by Frame   
- _, frame = camera.read()
- gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+ ret, im =camera.read()
+ gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
  faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
  for (x, y, w, h) in faces:
      print(x,y,w,h)
      roi_gray = gray[y:y+h, x:x+w]
-     roi_color = frame[y:y+h, x:x+w]
+     roi_color = im[y:y+h, x:x+w]
      
      #recognize?
-     id_,conf = recognizer.predict(roi_gray)
-     if conf>=45 and conf<=85:
-         #print(id_)
-      img_item = "my-image.png"
+     Id, conf = recognizer.predict(gray[y:y+h,x:x+w])
+     if(conf<55):
+        if(Id==1):
+          Id="Deo"
+        if(Id==2):
+          Id="name"
+        if(Id==4):
+          Id="name"
+         
+     else:
+          Id="Unknown"  
+
+     cv.putText(im,str(Id),(x,y+h),font,3,255)
+        
+     img_item = "my-image.png"
      cv.imshow(img_item, roi_gray)
      
-     cv.rectangle(frame, (x,y) , (x+w, y+h) , (255, 0, 0), 2)
+     cv.rectangle(im, (x,y) , (x+w, y+h) , (255, 0, 0), 3)
  
 #Display resulting Frame
- cv.imshow('frame', frame)
- if cv.waitKey(5) == ord('X'):
+ cv.imshow('im', im)
+ if cv.waitKey(10) & 0xFF==ord('q'):
    break
 #When everything done,release camera
 camera.release()
